4fc337b1240c971edfd911a488608ef2077d5b0f
dmitrii <dmitriilazukov@gmail.com>
2018-12-10 15:51:09 +0500

    Add GTM

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function (req, res) {
         'copyrightText': t('COPYRIGHT_TEXT'),
         'cookiesText': t('COOKIES_TEXT')
     });
+    let env = process.env;
     res.render('index', {title: t('WEBSITE_TITLE'), initialState: initialState});
 });
 
